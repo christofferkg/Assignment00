48dd9666653fc12b1c8ffa36320c140c6fee7ec0
Christoffer Gram <chkg@itu.dk>
2021-09-04 14:33:26 +0200

    Add UI + Implement Error Handling

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -7,40 +7,28 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void mainPrintsHelloWorld()
-        {
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-
-            Program.Main(new string[0]);
-            var output = writer.GetStringBuilder().ToString().Trim();
-
-            Assert.Equal("Hello, World!", output); 
-        }
-
-        [Fact]
-        public void IsLeapYearIsFalse() {
+        public void IsLeapYear_False() {
             int year = 2010;
             var output = Program.IsLeapYear(year);
             Assert.False(output);
         }
 
         [Fact]
-        public void DivisibleByFourIsLeapYear() {
+        public void IsLeapYear_DivisibleByFourTrue() {
             int year = 2012;
             var output = Program.IsLeapYear(year);
             Assert.True(output);
         }
 
         [Fact]
-        public void DivisibleBy400IsLeapYear() {
+        public void IsLeapYear_DivisibleBy400IsTrue() {
             int year = 1600;
             var output = Program.IsLeapYear(year);
             Assert.True(output);
         }
 
         [Fact]
-        public void DivisibleBy100IsNotLeapYear() {
+        public void IsLeapYear_DivisibleBy100IsFalse() {
             int year = 1900;
             var output = Program.IsLeapYear(year);
             Assert.False(output);
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,10 +6,24 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = GetUserInput();
+            bool isParsed = false;
+            while (!isParsed) {
+                int year;
+                isParsed = Int32.TryParse(input, out year);
+
+                if (isParsed) {
+                    PrintIsLeapYear(year);
+                    return;
+                }
+                input = GetUserInput();
+            }
         }
 
         public static bool IsLeapYear(int year) {
+            if (year < 1582) {
+                throw new ArgumentException("Year must pre-date 1582");
+            }
             if (year % 100 == 0 && year % 400 == 0) {
                 return true;
             } else if (year % 100 == 0) {
@@ -18,6 +32,29 @@ namespace MyApp
                 return true;
             }
             return false;
+        }
+
+        public static String GetUserInput() {
+            Console.WriteLine("Enter a year:");
+            Console.Write("> ");
+
+            return Console.ReadLine();
+        }
+
+        public static void PrintIsLeapYear(int year) {
+            bool isLeapYear;
+            try {
+                isLeapYear = IsLeapYear(year);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            if (IsLeapYear(year)) {
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("nay");
+            }
         }
     }
 }
